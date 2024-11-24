410d7af35285facf054feab752065c4c559ec638
TaylahFrederickSAE <1032576@student.sae.edu.au>
2024-11-24 20:36:39 +1100

    Began setting up card screen and experimenting with placement, edited the cardInfo scriptable objects

--- Assets/Scripts/CardInfo.cs
+++ Assets/Scripts/CardInfo.cs
@@ -6,9 +6,8 @@ public class FormStats : ScriptableObject
     //Hosts all of the following information for each form type: 
     public string cardName;
     public float damageOutput;
-    public float hitPoints;
-    public string abilityOne; 
-    public string abilityTwo;
+    public float hitChance;
+    public string abilityDescription; 
     public Sprite sprite; 
 
 }
